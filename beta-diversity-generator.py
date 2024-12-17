705ad24848bf7f5bf78715df382710b6b330b86a
Mario Cuaya <mariocuaya5@gmail.com>
2024-12-17 15:26:06 -0800

    Finishing beta diversity generator

--- beta-diversity-generator.py
+++ beta-diversity-generator.py
@@ -1,6 +1,5 @@
 import argparse
-import datetime as dt
-import pandas as pd
+from datetime import datetime
 import os
 from skbio import OrdinationResults
 from qiime2.plugins import feature_table
@@ -11,10 +10,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def signifcance_test(distance_matrix, output):
-    diversity.visualizers.adonis(distance_matrix=distance_matrix,
-                                permutations=999)
+def signifcance_test(distance_matrix, output, metadata, data_column):
+    print('Generating signifcance test...')
+    column = metadata.get_column(data_column)
+    permanova = diversity.visualizers.beta_group_significance(
+            distance_matrix=distance_matrix,
+            method='permanova',
+            metadata=column,
+            pairwise=True,
+            permutations=999)
+    permanova.visualization.save(f'{output}/signifcance_test.qzv')
 
+
+def stats_generator(stats, output):
+    colums_to_drop = stats.columns.to_list()
+    dataframe = stats.drop(columns=colums_to_drop[5:])
+    renumber_columns = dataframe.columns.to_list()
+    renumber_columns = [x + 1 for x in renumber_columns]
+    dataframe.columns = renumber_columns
+    print('Generating excel file...')
+    dataframe.to_excel(f'{output}beta_diversity_stats.xlsx')
+
+    print('Generating markdown file with table stats...')
+    time_generated=datetime.now().strftime("%d/%m/%y %H:%M:%S")
+    with open(f'{output}beta_diversity_stats.md', "w") as f:
+        f.write(f'''#Beta diversity stats\n
+                ## To find further sequence specific information, refer to table 03 generated previously\n
+                **Please refer to the excel or csv file generated to perform further analysis.**\n
+                Date file was generated: {time_generated}\n
+                {dataframe.to_markdown()}\n''')
+
+    print('Generating html file with table stats...')
+    with open(f'{output}beta_diversity_stats.html', "w") as f:
+        f.write(f'''<!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+            <h1>Beta diversity stats</h1>
+            <h2 >To find further sequence specific information, refer to table 03 generated previously.</h2>
+            <strong>Please refer to the excel file generated to perform further analysis. </strong>
+            <p>Date file was generated: {time_generated}</p>
+            {dataframe.to_html()}''')
 
 
 def beta_diversity(asv_table, map_file, data_column, treatments, plot_tilte, output):
@@ -31,10 +70,8 @@ def beta_diversity(asv_table, map_file, data_column, treatments, plot_tilte, out
             metric='braycurtis')
 
     beta_diversity_table = beta_results.distance_matrix
-    signifcance_test(beta_diversity_table, output)
 #    # https://forum.qiime2.org/t/load-distancematrix-artifact-to-dataframe/11660
-#    # Turn table into a distance matrix
-#
+    signifcance_test(beta_diversity_table, output, map_file, data_column)
     pcoa_results = diversity.methods.pcoa(distance_matrix=beta_diversity_table)
     pcoa_results = pcoa_results.pcoa
     pcoa_results = pcoa_results.view(OrdinationResults)
@@ -46,7 +83,7 @@ def beta_diversity(asv_table, map_file, data_column, treatments, plot_tilte, out
 # https://medium.com/@conniezhou678/applied-machine-learning-part-12-principal-coordinate-analysis-pcoa-in-python-5acc2a3afe2d
 # https://www.tutorialspoint.com/numpy/numpy_matplotlib.htm
     pcoa_results = pcoa_results.samples
-#    pcoa_results.columns = pcoa_results.index.to_list()
+    stats_generator(pcoa_results, output)
     fig, ax = plt.subplots(figsize=(15, 10))
     cmap = plt.get_cmap('tab20')
     colors = [cmap(i) for i in np.linspace(0, 1, len(treatments))]
@@ -64,12 +101,12 @@ def beta_diversity(asv_table, map_file, data_column, treatments, plot_tilte, out
 # https://stackoverflow.com/questions/13588920/stop-matplotlib-repeating-labels-in-legend
     handles, labels = plt.gca().get_legend_handles_labels()
     uniques = dict(zip(labels, handles))
-    ax.legend(uniques.values(), uniques.keys(), bbox_to_anchor=(1, 1), frameon=False, title="Treatments", alignment='left')
+    ax.legend(uniques.values(), uniques.keys(), bbox_to_anchor=(1, 1), frameon=False, title="Treatments", loc='upper left')
     plt.title(f'{plot_tilte}', fontsize='20') 
     ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+    ax.spines['right'].set_visible(False) 
+    fig.tight_layout()
     plt.grid(True)
-    plt.tight_layout()
     fig.savefig(f"{output}beta_diversity.png")
 
 #Further resources can be found at the following links below:
